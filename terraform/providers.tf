c4efbc780ba377d96855bef49e5d827fea1f69ae
ffgi <agilles91@gmail.com>
2024-05-02 00:05:23 +0200

    infra: add terraform configuration block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-westeurope"
+    storage_account_name = "csb100320037a918692"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
+    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+  }
+}
