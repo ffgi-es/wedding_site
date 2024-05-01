a455e984c0eb3f76f9e65d7de4729102b232a54b
ffgi <agilles91@gmail.com>
2024-05-01 23:38:21 +0200

    infra: add terraform

---
+++ terraform/app_service.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "wedding_site" {
+  name     = "wedding-site"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "wedding_site" {
+  name                = "wedding-site-plan"
+  resource_group_name = azurerm_resource_group.wedding_site.name
+  location            = azurerm_resource_group.wedding_site.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "wedding_site" {
+  name                = "wedding-site-app"
+  resource_group_name = azurerm_resource_group.wedding_site.name
+  location            = azurerm_resource_group.wedding_site.location
+  service_plan_id     = azurerm_service_plan.wedding_site.id
+
+  site_config {
+    always_on = false
+    application_stack {
+      docker_image_name        = "${var.docker_username}/wedding_site:${var.image_tag}"
+      docker_registry_url      = "https://index.docker.io"
+      docker_registry_username = var.docker_username
+      docker_registry_password = var.docker_token
+    }
+
+    websockets_enabled = true
+    use_32_bit_worker  = false
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "image_tag" {
+  type = string
+}
+
+variable "docker_username" {
+  type = string
+}
+
+variable "docker_token" {
+  type = string
+}
