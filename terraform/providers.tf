e7609897a02025e9534ebf1807565431c33eeef4
ffgi <agilles91@gmail.com>
2024-08-17 18:40:29 +0200

    fix naming and update terraform version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
     storage_account_name = "tfstate29597"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
-    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    use_oidc             = true # Can also be set via `ARM_USE_OIDC` environment variable.
+    use_azuread_auth     = true # Can also be set via `ARM_USE_AZUREAD` environment variable.
   }
 }
