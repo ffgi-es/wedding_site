489a0cb570941e10464d62cedac4cfbfa529a397
ffgi <agilles91@gmail.com>
2024-09-23 23:15:28 +0200

    add initial qr generation console application

---
+++ generate_rsvp/Program.cs
@@ -0,0 +1,15 @@
+﻿using QRCoder;
+
+var rsvp = Random.Shared.Next(100_000, 1_000_000).ToString();
+
+var passcode = Random.Shared.Next(1_000, 10_000).ToString();
+
+var data = new PayloadGenerator.Url($"https://emilie-alastair-wedding.azurewebsites.net/RSVP/{rsvp}").ToString();
+
+using var qrCodeGenerator = new QRCodeGenerator();
+using var qrCodeData = qrCodeGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+using var ascii = new AsciiQRCode(qrCodeData);
+
+var output = ascii.GetGraphic(1);
+
+Console.WriteLine(output);
