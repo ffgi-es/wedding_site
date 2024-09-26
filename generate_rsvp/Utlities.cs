718e8d327aabef8ae6b4064fa3d8fa8b059146a7
ffgi <agilles91@gmail.com>
2024-09-26 20:39:00 +0200

    refactor style of braces

--- generate_rsvp/Utlities.cs
+++ generate_rsvp/Utlities.cs
@@ -5,11 +5,16 @@ using wedding_site.Domain;
 
 namespace wedding_site.RsvpGeneration;
 
-public class Utilities {
+public class Utilities
+{
     public static (string Rsvp, string Passcode) GenerateRsvpCredentials() =>
-        (Random.Shared.Next(100_000, 1_000_000).ToString(), Random.Shared.Next(1_000, 10_000).ToString());
+        (
+            Random.Shared.Next(100_000, 1_000_000).ToString(),
+            Random.Shared.Next(1_000, 10_000).ToString()
+        );
 
-    public static async Task CreateRsvpInCosmos(string rsvp, string passcode) {
+    public static async Task CreateRsvpInCosmos(string rsvp, string passcode)
+    {
         var cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("COSMOS_CONNECTIONSTRING"));
 
         var rsvpRepo = new RsvpRepo(cosmosClient);
@@ -17,7 +22,8 @@ public class Utilities {
         await rsvpRepo.SaveRsvp(new Rsvp(rsvp, passcode));
     }
 
-    public static byte[] GenerateQrCodeForUrl(string url, int pixels) {
+    public static byte[] GenerateQrCodeForUrl(string url, int pixels)
+    {
         var data = new PayloadGenerator.Url(url).ToString();
 
         using var qrCodeGenerator = new QRCodeGenerator();
@@ -25,9 +31,5 @@ public class Utilities {
         using var qrCode = new BitmapByteQRCode(qrCodeData);
 
         return qrCode.GetGraphic(pixels);
-    }
-
-    public static void AddRsvpTextToQr(string rsvp) {
-
     }
 }
